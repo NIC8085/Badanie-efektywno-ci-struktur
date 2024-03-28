f850301bb6f55af1846deb6ed8482d0c1a0785c9
kacper <kacper.markowski2@teb.edu.pl>
2024-03-28 20:15:02 +0100

    lekka naprawa maina

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -126,7 +124,7 @@ public class Main {
                 usingFunctionsSinglyLinkedList();
             }
             case 2 -> {
-                myArray.remove(givenValue);
+                myLikedList.remove(givenValue);
                 myLikedList.print();
                 System.out.println(" ");
                 choosingOperation();
