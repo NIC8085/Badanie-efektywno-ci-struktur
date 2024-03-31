e3a540977006689f0b894adaf76a4da3130854f3
kacper <kacper.markowski2@teb.edu.pl>
2024-03-31 12:28:15 +0200

    zmiany z dodawaniem z pliku

--- src/FileReader.java
+++ src/FileReader.java
@@ -13,7 +13,8 @@ public class FileReader {
     static long end_time = System.currentTimeMillis();
     static int getValue;
     static int exc_time = 0;
-    static File plik = new File("C:\\Users\\kacpe\\Documents\\szkoła\\java\\BadanieEfektywności\\BadanieEfektywności\\src\\file.txt");
+    static int wantedSize = 100000;
+    static File plik = new File("src/file.txt");
     public static void main(String[] args) throws FileNotFoundException {
         addingToArray();
         addingToLinkedList();
@@ -24,10 +25,11 @@ public class FileReader {
         readFile = new Scanner(plik);
         int index = 0;
         start_time = System.currentTimeMillis();
-        while(readFile.hasNext()){
-            getValue = readFile.nextInt();
-            myArray.add(index, getValue);
-            index++;
+        for (int i = 0; i<wantedSize; i++){
+            while(readFile.hasNext()){
+                getValue = readFile.nextInt();
+                myArray.add(i, getValue);
+            }
         }
         end_time = System.currentTimeMillis();
         exc_time = (int)(end_time-start_time);
@@ -36,12 +38,12 @@ public class FileReader {
     public static void addingToLinkedList() throws FileNotFoundException {
         Scanner readFile;
         readFile = new Scanner(plik);
-        int index = 0;
         start_time = System.currentTimeMillis();
-        while(readFile.hasNext()){
-            getValue = readFile.nextInt();
-            myLikedList.add(index, getValue);
-            index++;
+        for (int i = 0; i<wantedSize; i++) {
+            while (readFile.hasNext()) {
+                getValue = readFile.nextInt();
+                myLikedList.add(i, getValue);
+            }
         }
         end_time = System.currentTimeMillis();
         exc_time = (int)(end_time-start_time);
@@ -52,13 +54,15 @@ public class FileReader {
         readFile = new Scanner(plik);
         int index = 0;
         start_time = System.currentTimeMillis();
-        while(readFile.hasNext()){
-            getValue = readFile.nextInt();
-            myDoublyLinkedList.add(index, getValue);
-            index++;
+        for (int i = 0; i<wantedSize; i++) {
+            while (readFile.hasNext()) {
+                getValue = readFile.nextInt();
+                myDoublyLinkedList.add(index, getValue);
+            }
         }
         end_time = System.currentTimeMillis();
         exc_time = (int)(end_time-start_time);
+        myDoublyLinkedList.print();
         System.out.println("\nCzas dodawania do listy dwukierunkowej od początku wynosi: "+exc_time+" ms");
     }
 }
